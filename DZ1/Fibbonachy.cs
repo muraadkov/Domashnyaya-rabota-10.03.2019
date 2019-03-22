71772d48147fd1ce50a552090a560977abf4be04
Адылканов Муратжан Талгатович <murka_505853@mail.ru>
2019-03-23 05:16:14 +0600

    Домашняя работа

---
+++ DZ1/Fibbonachy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Fibbonachy
+    {
+        public void FibbonachyInText()
+        {
+            List<int> strings = new List<int>();
+            var loadFile = new StreamReader("fibbonachy.txt");
+            string _string = "";
+            while (!loadFile.EndOfStream)
+            {
+                _string = loadFile.ReadToEnd();
+                string[] splitString = _string.Split(',');
+                for (int i = 0; i < splitString.Count(); i++)
+                {
+                    strings.Add(int.Parse(splitString[i]));
+                }
+            }
+            loadFile.Close();
+            int countElements = strings.Count;
+            for (int i = 0; i < countElements; i++)
+            {
+                strings.Add(strings[strings.Count - 1] + strings[strings.Count - 2]);
+            }
+            foreach (var c in strings)
+            {
+                Console.Write(c.ToString() + " ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DZ2/PlusNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2
+{
+    class PlusNumbers
+    {
+        public void PlusNumbersInTextFile()
+        {
+            List<int> strings = new List<int>();
+            var loadFile = new StreamReader("slozhenie.txt");
+            string _string = "";
+            while (!loadFile.EndOfStream)
+            {
+                _string = loadFile.ReadToEnd();
+                string[] splitString = _string.Split(' ');
+                for (int i = 0; i < splitString.Count(); i++)
+                {
+                    strings.Add(int.Parse(splitString[i]));
+                }
+            }
+            loadFile.Close();
+            var saveFile = new StreamWriter("resultat.txt");
+            saveFile.Write(strings.Sum());
+            saveFile.Close();
+            Console.ReadKey();
+        }
+    }
+}
